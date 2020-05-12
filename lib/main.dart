99ccc32a8202965b955b27a5b07631b29e4f79b7
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-05-12 20:12:01 +0700

    delegate

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_bloc_counter/bloc/counter_bloc.dart';
+import 'package:flutter_bloc_counter/logger/counter_bloc_delegate.dart';
 import 'package:flutter_bloc_counter/page/counter_page.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  BlocSupervisor.delegate = CounterBlocDelegate();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -17,5 +21,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-
